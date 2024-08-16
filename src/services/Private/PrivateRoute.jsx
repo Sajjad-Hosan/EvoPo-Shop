6e7d6e4dfe9793e972f30ece06cd52d82201156c
Sajjad-Hosan <s4jj4dho34n@hmail.com>
2024-08-17 03:45:50 +0600

    services

---
+++ src/services/Private/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import { Navigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  if (loading)
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <spam className="loading loading-dots loading-lg"></spam>
+      </div>
+    );
+  if (!user) {
+    return <Navigate to="/login" replace={true} />;
+  } else <>{children}</>;
+};
+
+export default PrivateRoute;
